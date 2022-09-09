ca4ec3b9525eeb0ab2aa4ed274cda68ef4a061e5
cugbo <chimaugbo@gmail.com>
2022-09-09 14:49:17 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-	int n, m, 1;
+	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (1 = 50; 1 < 58; 1++)
+			for (l = 50; l < 58; l++)
 			{
-				if (1 > m && m > n)
+				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
-					putchar(1);
+					putchar(l);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
