a3c72267edc7ea4bc6d9bb9a41a3cd0f91a24cf7
cugbo <chimaugbo@gmail.com>
2022-09-13 14:24:14 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -22,7 +22,7 @@ int main(void)
 			hold3 = fr1 + fr2 + hold1;
 			fr1 = fr2, fr2 = hold3;
 			bk1 = bk2, bk2 = hold2;
-			printf("%lu%0101lu", fr2, bk2);
+			printf("%lu%010lu", fr2, bk2);
 		}
 		else
 		{
