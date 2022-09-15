b7fb390dc3e18df818572cd26b2ce6cc18a99551
cugbo <chimaugbo@gmail.com>
2022-09-15 05:24:46 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: can only use _putchar 3 timess
+ */
+void more_numbers(void)
+{
+	int c, i;
+
+	c = 0;
+	while (c < 10)
+	{
+		i = o;
+		while (i < 15)
+		{
+			if (1 > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			i++;
+		}
+		c++;
+		_putchar('\n');
+	}
+}
