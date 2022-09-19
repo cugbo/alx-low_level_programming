7feeeb3715c4f313d7de7d8a17afd857171172ae
cugbo <chimaugbo@gmail.com>
2022-09-19 12:32:22 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,21 +5,16 @@
  */
 void rev_string(char *s)
 {
-	char *t = s;
-	char n[1000];
-	short c = 0;
+	int i, c, l;
+	char h;
 
-	while (*s != '\n')
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	l = i;
+	for (i--, c = 0; c < l / 2; i--, c++)
 	{
-		n[c] = *s;
-		s++;
-		c++;
-	}
-	c = 0;
-	while (s > t)
-	{
-		s--;
-		*s = n[c];
-		c++;
+		h = s[c];
+		s[c] = s[i];
+		s[i] = h;
 	}
 }
