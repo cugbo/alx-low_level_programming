2d0a42b0732773e87392599edf5a2817eeb7c547
cugbo <chimaugbo@gmail.com>
2022-09-19 12:43:23 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,16 +5,12 @@
  */
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		i++;
-	}
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
