18ac4c582bcd6da3591e7e76b087d9fa4d8aa620
cugbo <chimaugbo@gmail.com>
2022-09-21 13:57:27 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string starting from index 0 of 'dest'
+ * @dest: string
+ * @src: string
+ * @n: number of chars to copy
+ * Return: 'dest' edited string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; n > i; i++)
+		dest[i] = '\0';
+	return (dest);
+}
