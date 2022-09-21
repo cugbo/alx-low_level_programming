62f0f9fde9c31060572ce59381bea06c687e5d30
cugbo <chimaugbo@gmail.com>
2022-09-21 13:24:15 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -9,3 +11,4 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+#endif
