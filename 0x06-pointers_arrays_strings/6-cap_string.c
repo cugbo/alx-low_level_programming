296c50270665fcb72c5263ae26e179ccecd5bb21
cugbo <chimaugbo@gmail.com>
2022-09-21 15:08:18 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,16 +7,17 @@
 char *cap_string(char *s)
 {
 	int i, j;
-	char delimeters[13] = {' ', '\t', '\n', ',', ';', '.',
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0; s[i] >= 'a' && s[i] <= 'z')
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
 		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == delimeters[j])
+			if (s[i] == spe[j])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
