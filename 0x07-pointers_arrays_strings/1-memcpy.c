e9a8347165b24b549fbf34cf04623ac3d6ff9d73
cugbo <chimaugbo@gmail.com>
2022-09-26 16:03:37 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,15 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *yolo;
+	unsigned int i;
 
-	yolo = dest;
-	while (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
+		dest[i] = src[i];
 	}
-	return (yolo)
+	return (dest);
 }
