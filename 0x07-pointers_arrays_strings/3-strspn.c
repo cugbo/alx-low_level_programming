2feb2af806a5f149aa05aa83f2c5753cc9feaec9
cugbo <chimaugbo@gmail.com>
2022-09-26 16:18:09 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strspn - search a string for a set of bytes
+ * @s: char string array
+ * @accept: char array to check byte with
+ * Return: number of bytes in the inital segment of 's'
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int c;
+
+	i = 0;
+	c = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
+			break;
+		i++;
+	}
+	return (c);
+}
