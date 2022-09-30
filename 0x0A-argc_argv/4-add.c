3233e507ae1b10e88aa9b68f7b7fbd4c3be45c94
cugbo <chimaugbo@gmail.com>
2022-09-30 15:01:46 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,30 +11,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, length, sum;
-	char *ptr;
+	int a = 0, i, j;
 
-	if (args < 2)
-		printf("0\n");
-	else
+	for (i = 1; i < argc; i++)
 	{
-		sum = 0;
-
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			ptr = argv[i];
-			length = strlen(ptr);
-			for (j = 0; j < length; j++)
+			if (isdigit(argv[i][j]) == 0)
 			{
-				if (isdigit(*(ptr + j)) == 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
+				puts("Error");
+				return (1);
 			}
-			sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
 	return (0);
 }
