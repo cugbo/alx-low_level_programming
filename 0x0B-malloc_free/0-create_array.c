0b4d5961e90df4f66bca687e6206c1ead16d76cf
cugbo <chimaugbo@gmail.com>
2022-10-03 02:28:24 -0700

    1-strdup.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int index;
 
-	if (size == o)
+	if (size == 0)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
 	if (array == NULL)
