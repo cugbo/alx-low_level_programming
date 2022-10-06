16a5d4b63a75d701fcccab35382de33d1b5c8c64
cugbo <chimaugbo@gmail.com>
2022-10-06 09:59:08 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,21 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *mem;
-	char filler;
+	char *filler;
 	unsigned int index;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	mem = malloc(size * nmemb);
+
 	if (mem == NULL)
 		return (NULL);
+
 	filler = mem;
+
 	for (index = 0; index < (size * nmemb); index++)
 		filler[index] = '\0';
+
 	return (mem);
 }
