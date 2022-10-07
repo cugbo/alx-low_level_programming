c2a10a8efbf2802d82f72c3537da2201073a15e5
cugbo <chimaugbo@gmail.com>
2022-10-06 21:04:38 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -32,7 +32,7 @@ int _strlen(char *s)
 	{
 		i++;
 	}
-	return (1);
+	return (i);
 }
 /**
  * errors - handles error for main
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digits(s1) || !is_digit(s2))
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
