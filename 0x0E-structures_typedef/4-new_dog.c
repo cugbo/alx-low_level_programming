990e7ef257ec16f698f48938838a7d87e61eb5bf
cugbo <chimaugbo@gmail.com>
2022-10-10 09:47:04 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,13 +8,12 @@ dog_t *new_dog(char *name, float age, char *owner);
 /**
  * _strlen - Finds the length of a string.
  * @str: The string to be measured.
- *
  * Return: The length of the string.
  */
 int _strlen(char *str)
 {
 	int len = 0;
-	
+
 	while (*str++)
 		len++;
 	return (len);
@@ -25,13 +24,12 @@ int _strlen(char *str)
  *            terminating null byte, to a buffer pointed to by dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
- *
  * Return: The pointer to dest.
  */
 char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
-	
+
 	for (index = 0; src[index]; index++)
 		dest[index] = src[index];
 	dest[index] = 0;
@@ -43,13 +41,12 @@ char *_strcopy(char *dest, char *src)
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
- *
  * Return: The new struct dog.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
-	
+
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 	doggo = malloc(sizeof(dog_t));
