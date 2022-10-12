10d84b9a4500f466de414f3692f1b9d8b85ccb72
cugbo <chimaugbo@gmail.com>
2022-10-12 14:12:11 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,44 +1,37 @@
-#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
- * Return: If new_size == old_size - ptr.
- *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
+ * main - Prints the opcodes of itself.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int main(int argc, char *argv[])
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
+	if (argc != 2)
 	{
-		mem = malloc(new_size);
-		if (mem == NULL)
-			return (NULL);
-		return (mem);
+		printf("Error\n");
+		exit(1);
 	}
-	if (new_size == 0 && ptr != NULL)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
-		free(ptr);
-		return (NULL);
+		printf("Error\n");
+		exit(2);
 	}
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
-	if (mem == NULL)
+	for (index = 0; index < bytes; index++)
 	{
-		free(ptr);
-		return (NULL);
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+		address++;
 	}
-	filler = mem;
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
-	free(ptr);
-	return (mem);
+	printf("\n");
+	return (0);
 }
