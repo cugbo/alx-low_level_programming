6c38e857ac300754aa0369a346cbf1a9c6d21136
cugbo <chimaugbo@gmail.com>
2022-10-13 12:48:30 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,6 +11,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_list list;
 	int sum;
 
+	if (n == 0)
+		return (0);
 	va_start(list, n);
 	for (i = 0, sum = 0; i < n; i++)
 		sum += va_arg(list, int);
